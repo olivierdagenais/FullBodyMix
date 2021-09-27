447eeb2fe338d80aa7dd7b3774c37d26f76f91cb
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-09-27 19:01:23 -0400

    refactor: extract Burpees "constant"

--- FullBodyMix.Test/Models/ExerciseTest.cs
+++ FullBodyMix.Test/Models/ExerciseTest.cs
@@ -9,21 +9,23 @@ namespace FullBodyMix.Models
 	[TestClass]
 	public class ExerciseTest
 	{
-		[TestMethod]
-		public void SerializationRoundTrip()
+		public static readonly Exercise Burpees = new()
 		{
-			var exercise = new Exercise
-			{
-				Name = "Burpees",
-				FocusArea = FocusArea.Cardio,
-				Description = @"Start standing up
+			Name = "Burpees",
+			FocusArea = FocusArea.Cardio,
+			Description = @"Start standing up
 1. Crouch down
 2. Kick your feet backwards to get into a high plank
 3. Perform a push up
 4. Bring your feet back into a crouching position
 5. Stand up
 6. Repeat",
-			};
+		};
+
+		[TestMethod]
+		public void SerializationRoundTrip()
+		{
+			var exercise = Burpees;
 			var exerciseAsString = JsonSerializer.Serialize(exercise);
 
 			var actual = JsonSerializer.Deserialize<Exercise>(exerciseAsString);
