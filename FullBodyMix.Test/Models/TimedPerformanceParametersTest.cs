fa682df5203173fc45c9d41e2c48a57ea36bbb13
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-09-27 18:57:55 -0400

    feat: renovate TimedWorkout[Test]
    Rather than being a type of workout, it's going to be a type of
    performance on a per-exercise basis.

---
+++ FullBodyMix.Test/Models/TimedPerformanceParametersTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FullBodyMix.Models
+{
+	/// <summary>
+	/// A class to test <see cref="TimedPerformanceParameters"/>.
+	/// </summary>
+	[TestClass]
+	public class TimedPerformanceParametersTest
+	{
+		[TestMethod]
+		public void SerializationRoundTrip()
+		{
+			var parameters = new TimedPerformanceParameters
+			{
+				RestTime = TimeSpan.FromSeconds(15),
+				WorkTime = TimeSpan.FromSeconds(45),
+			};
+			var parametersAsString = JsonSerializer.Serialize(parameters);
+
+			var actual = JsonSerializer.Deserialize<TimedPerformanceParameters>(parametersAsString);
+
+			Assert.AreEqual(parameters.RestTime, actual.RestTime);
+			Assert.AreEqual(parameters.WorkTime, actual.WorkTime);
+		}
+	}
+}
--- FullBodyMix/Models/TimedPerformanceParameters.cs
+++ FullBodyMix/Models/TimedPerformanceParameters.cs
@@ -3,14 +3,8 @@ using System.Text.Json.Serialization;
 
 namespace FullBodyMix.Models
 {
-	public record TimedWorkout : Workout
+	public record TimedPerformanceParameters
 	{
-		/// <summary>
-		/// How long would this workout take to perform?
-		/// </summary>
-		[JsonConverter(typeof(JsonTimeSpanConverter))]
-		public TimeSpan Duration { get; init; }
-
 		/// <summary>
 		/// How long is the pause until the next exercise?
 		/// </summary>
@@ -18,7 +12,7 @@ namespace FullBodyMix.Models
 		public TimeSpan RestTime { get; init; }
 
 		/// <summary>
-		/// How long should each exercise be performed for?
+		/// How long should the exercise be performed for?
 		/// </summary>
 		[JsonConverter(typeof(JsonTimeSpanConverter))]
 		public TimeSpan WorkTime { get; init; }
