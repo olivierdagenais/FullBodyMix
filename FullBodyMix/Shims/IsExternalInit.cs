4389405ce29962257cafe2d908734a522afe71f3
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-10-06 22:28:36 -0400

    feat: upgrade to .NET 5.0

--- FullBodyMix/Shims/IsExternalInit.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System.Runtime.CompilerServices
-{
-	/// <summary>
-	/// C# 9.0 adds the "record" type but init-only properties need .NET 5.0 OR this class.
-	/// </summary>
-	/// <seealso href="https://stackoverflow.com/a/64749403/98903"/>
-	internal static class IsExternalInit { }
-}
