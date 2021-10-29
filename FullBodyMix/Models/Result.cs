60c6b68cdfb7e600f6b5e5417826694b78232a47
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-10-29 15:38:08 -0400

    feat: add Result

---
+++ FullBodyMix/Models/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace FullBodyMix.Models
+{
+	public enum Result
+	{
+		/// <summary>
+		/// Keep going.
+		/// </summary>
+		Continue,
+
+		/// <summary>
+		/// Stop everything.
+		/// </summary>
+		Cancel,
+
+		// TODO: pause, skip, etc.?
+	}
+}
