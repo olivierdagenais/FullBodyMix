38378060a1e1535725667c1052f3c71948226739
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-10-29 14:18:19 -0400

    feat: initial ViewParameters

---
+++ FullBodyMix/Models/ViewParameters.cs
@@ -0,0 +1,31 @@
+﻿namespace FullBodyMix.Models
+{
+	public record ViewParameters
+	{
+		/// <summary>
+		/// What <see cref="Mode"/> are we currently at?
+		/// </summary>
+		public Mode CurrentMode { get; init; }
+
+		/// <summary>
+		/// What <see cref="Exercise"/> and <see cref="PerformanceParameters"/>
+		/// are we currently at?
+		/// </summary>
+		public PlaylistEntry CurrentEntry { get; init; }
+
+		/// <summary>
+		/// The progress of the current <see cref="PlaylistEntry"/>.
+		/// </summary>
+		public string CurrentProgress { get; init; }
+
+		/// <summary>
+		/// The progress across the entire <see cref="Workout"/>.
+		/// </summary>
+		public string OverallProgress { get; init; }
+
+		/// <summary>
+		/// Is there any text we want to hear?
+		/// </summary>
+		public string SpokenAnnouncement { get; init; }
+	}
+}
