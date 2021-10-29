6769ec17512d637d4684e2c4b9d5c83015c879bd
Olivier Dagenais <olivier.dagenais@gmail.com>
2021-10-29 13:51:08 -0400

    feat: add the Mode enum

---
+++ FullBodyMix/Models/Mode.cs
@@ -0,0 +1,15 @@
+﻿namespace FullBodyMix.Models
+{
+	public enum Mode
+	{
+		/// <summary>
+		/// Resting and getting ready to perform the next exercise.
+		/// </summary>
+		Preparing,
+
+		/// <summary>
+		/// During the exercise performance.
+		/// </summary>
+		Performing,
+	}
+}
