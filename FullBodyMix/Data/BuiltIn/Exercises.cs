897412783875c0fd058412d157194b557ceccead
Olivier Dagenais <olivier.dagenais@gmail.com>
2022-01-10 21:08:28 -0500

    feat: create Exercises from ExerciseTest
    This will allow test data to remain static for tests, without risking
    changes to exploratory/end-to-end test data to risk breaking assertions.

---
+++ FullBodyMix/Data/BuiltIn/Exercises.cs
@@ -0,0 +1,48 @@
+﻿using FullBodyMix.Models;
+
+namespace FullBodyMix.Data.BuiltIn
+{
+	/// <summary>
+	/// Hardcoded exercises, mostly to test with.
+	/// </summary>
+	internal class Exercises
+	{
+		public static readonly Exercise ArmCircles = new()
+		{
+			Name = "Arm circles",
+			FocusArea = FocusArea.Upper,
+		};
+
+		public static readonly Exercise Burpees = new()
+		{
+			Name = nameof(Burpees),
+			FocusArea = FocusArea.Cardio,
+			Description = @"Start standing up
+1. Crouch down
+2. Kick your feet backwards to get into a high plank
+3. Perform a push up
+4. Bring your feet back into a crouching position
+5. Stand up
+6. Repeat",
+		};
+
+		public static readonly Exercise Crunches = new()
+		{
+			Name = nameof(Crunches),
+			FocusArea = FocusArea.Middle,
+		};
+
+		public static readonly Exercise Lunges = new()
+		{
+			Name = nameof(Lunges),
+			Sides = 2,
+			FocusArea = FocusArea.Lower,
+		};
+
+		public static readonly Exercise Squats = new()
+		{
+			Name = nameof(Squats),
+			FocusArea = FocusArea.Lower,
+		};
+	}
+}
